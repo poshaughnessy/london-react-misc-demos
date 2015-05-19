5dcb3ef8eaf1ff9352fe27e1c0194df5dda19ecb
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2015-05-19 22:16:08 +0100

    Add React logo texture

--- examples/js/01_hellothreejs.js
+++ examples/js/01_hellothreejs.js
@@ -7,7 +7,6 @@
 
     // Create a WebGL renderer
     var renderer = new THREE.WebGLRenderer();
-
     renderer.setSize( width, height );
 
     // Add generated <canvas> to page
@@ -30,10 +29,13 @@
     // Add it to the scene
     scene.add( camera );
 
+    // Set up a texture
+    var texture = THREE.ImageUtils.loadTexture('textures/react-logo.jpg');
+
     // Make a cube
     var cube = new THREE.Mesh(
             new THREE.CubeGeometry( 50, 50, 50 ),
-            new THREE.MeshBasicMaterial( {color: 0xFF0000} ));
+            new THREE.MeshBasicMaterial( {map: texture} ));
 
     // Add it to the scene
     scene.add( cube );
@@ -42,7 +44,7 @@
     function animate() {
 
         // Angles are in radians
-        cube.rotation.y += 0.1;
+        cube.rotation.y += 0.05;
 
         // Re-render
         renderer.render(scene, camera);
